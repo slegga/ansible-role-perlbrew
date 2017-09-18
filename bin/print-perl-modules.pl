4d82306c1aca087bb3d2691fb18deb1b60f1b79f
slegga <steihamm@gmail.com>
2017-09-18 06:05:09 +0200

    Add perl modules

---
+++ bin/print-perl-modules.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+use YAML qw /LoadFile/;
+use warnings;
+use strict;
+use autodie;
+use 5.016;
+
+=head1 NAME
+
+print-perl-modules
+
+=head1 DESCRIPTION
+
+Print perl modules to screen.
+Used for updateing perlbrew version manually.
+
+Like this:
+ perlbrew switch perl-5.26.0
+ ./print-perl-modules.pl | perlbrew exec --with perl-5.26.0 cpanm
+
+=cut
+
+my $yaml = YAML::LoadFile("$ENV{HOME}/git/ansible-cloudatcost/roles/yum/vars/main.yml");
+for my $val (@{$yaml->{perlmodules}}) {
+	say $val;
+}
